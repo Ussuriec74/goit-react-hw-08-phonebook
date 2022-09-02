306bd22444279f897d622d2bb02dcd2d1a25596f
Valeriy Semko <valsemko74@gmail.com>
2022-09-02 10:06:04 +0300

    remake App

--- src/components/App.js
+++ src/components/App.js
@@ -8,36 +8,57 @@ import { Filter } from 'components/Filter/Filter';
 
 export const App = () => {
   const [contacts, setContacts] = useState(
-    [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ]
+    () => JSON.parse(localStorage.getItem('contacts')) ?? []
   );
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    const sevedContacts = JSON.parse(localStorage.getItem('contacts'));
-
-    if (sevedContacts) {
-      setContacts(sevedContacts);
-    }
-  }, []);
-
-  useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
+
+  const deleteContact = ( contactId ) => {
+     setContacts(
+       contacts => contacts.filter(contact => contact.id !== contactId),
+     )
+   }
   
+  const addContact = data => {
+    const existedName = contacts.find(
+      el => el.name.toLowerCase() === data.name.toLowerCase()
+    );
+    if (existedName)
+      return alert(
+        `${existedName.name} is already in contacts`
+      );
+
+    data.id = nanoid();
+    setContacts(
+      contacts => [data,...contacts]
+    );
+  }
+
+  const changeFilter = (evt) => {
+    setFilter({ filter: evt.currentTarget.value });
+  }
+  // const normalazedFilter = filter.toLowerCase();
+
+  const getFilteredContacts = () => {
+    
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()));
+      // contact.name.toLowerCase().includes(filter.toLowerCase()));
+  }
+
+  const filteredContacts = getFilteredContacts();
 
     return (
       <Box>
         <Section title="Phonebook">
-          <InputForm addContact={this.addContact} />
+          <InputForm addContact={addContact} />
         </Section>
         <Section title="Contacts">
-          <Filter value={filter} onChange={this.changeFilter} />
-          <ContactList contacts={filteredContacts} onDeleteContact={this.deleteContact} />
+          <Filter value={filter} onChange={changeFilter} />
+          <ContactList contacts={filteredContacts} onDeleteContact={deleteContact} />
         </Section>
       </Box>
     );
@@ -77,34 +98,34 @@ export const App = () => {
 //     }))
 //   }
 
-//   addContact = data => {
-//     const existedName = this.state.contacts.find(
-//       el => el.name.toLowerCase() === data.name.toLowerCase()
-//     );
-//     if (existedName)
-//       return alert(
-//         `${existedName.name} is already in contacts`
-//       );
+  // addContact = data => {
+  //   const existedName = this.state.contacts.find(
+  //     el => el.name.toLowerCase() === data.name.toLowerCase()
+  //   );
+  //   if (existedName)
+  //     return alert(
+  //       `${existedName.name} is already in contacts`
+  //     );
 
-//     data.id = nanoid();
-//     this.setState(prevState => ({
-//       contacts: [data,...prevState.contacts]
+  //   data.id = nanoid();
+  //   this.setState(prevState => ({
+  //     contacts: [data,...prevState.contacts]
 
-//     }));
-//   }
+  //   }));
+  // }
 
-//   changeFilter = (evt) => {
-//     this.setState({ filter: evt.currentTarget.value });
-//   }
+  // changeFilter = (evt) => {
+  //   this.setState({ filter: evt.currentTarget.value });
+  // }
 
-//   getFilteredContacts = () => {
-//     const { contacts, filter } = this.state;
-//     const normalazedFilter = filter.toLowerCase();
+  // getFilteredContacts = () => {
+  //   const { contacts, filter } = this.state;
+  //   const normalazedFilter = filter.toLowerCase();
 
-//     return contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(normalazedFilter));
+  //   return contacts.filter(contact =>
+  //     contact.name.toLowerCase().includes(normalazedFilter));
 
-//   }
+  // }
 
 //   render() {
 //     const { filter } = this.state; 
