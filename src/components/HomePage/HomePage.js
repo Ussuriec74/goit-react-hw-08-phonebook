52d7c250613ad9546b5adfb476d16a7c8d029807
Valeriy Semko <valsemko74@gmail.com>
2022-10-06 15:36:41 +0300

    Update HomePage.js

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,7 +3,7 @@ import { HomePageTitle } from "./HomePage.styled"
 export const HomePage = () => {
   return (
     <HomePageTitle>
-      The phone book manager welcomes you. If you are registered, login, if not, register.
+      The phone book manager welcomes you.<br/>If you are registered, login,<br/> if not, register.
     </HomePageTitle>
   );
 }
