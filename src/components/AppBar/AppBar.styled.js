224c48ae7e88cfad2da6fda07bd46e7d9031a269
Valeriy Semko <valsemko74@gmail.com>
2022-10-06 12:45:46 +0300

    Update AppBar.styled.js

--- src/components/AppBar/AppBar.styled.js
+++ src/components/AppBar/AppBar.styled.js
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { NavLink } from "react-router-dom";
 
 export const Header = styled.header`
   top: 0;
@@ -7,23 +6,10 @@ export const Header = styled.header`
   position: sticky;
   z-index: 1100;
   display: flex;
-  justify-content: flex-start;
+  justify-content: space-between;
   align-items: center;
   width: 100%;
   min-height: ${p => p.theme.space[6]}px;
   padding: ${p => p.theme.space[3]}px ${p => p.theme.space[4]}px;
   background-color: ${p => p.theme.colors.accentColor};
-`;
-
-export const HeaderNavLink = styled(NavLink)`
-  display:inline-block;
-  text-decoration: none;
-  padding: ${p => p.theme.space[4]}px;
-  font-size: ${p => p.theme.fontSizes.m};
-  font-weight:${p => p.theme.fontWeights.normal};
-  color:${p => p.theme.colors.white};
-
-  &.active {
-    color: ${p => p.theme.colors.black};;
-  }
 `;
